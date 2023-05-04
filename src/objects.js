e4f4e5b5b375852a526d3ed7a74ea76ab7ab3187
Jonnie <jonniet23@hotmail.com>
2023-05-04 20:44:19 +0100

    objects tests completed

--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,64 @@
 const createPerson = (name, age) => {
-  // your code here
+  return {name, age
+  };
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
-};
+  if (object.hasOwnProperty(property)){
+  return true;
+  }
+  else {
+    return false;
+  }
+  };
+
 
 const isOver65 = person => {
-  // your code here
-};
+  if (person.age > 65) {
+    return true;
+  } else if(person.age <= 65) {
+    return false;
+  }
+  };
 
 const getAges = people => {
-  // your code here
-};
+  return people.map(person => person.age);
+  };
 
 const findByName = (name, people) => {
-  // your code here
-};
+  const person = people.find(someone => someone.name === name);
+  return person;
+    
+  };
+
 
 const findHondas = cars => {
-  // your code here
-};
+   return cars.filter(type => type.manufacturer ==='Honda');
+  };
+
 
 const averageAge = people => {
-  // your code here
+  return people.reduce((previous, person) => previous + person.age, 0) / people.length;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  const person ={
+    name,
+    age
+  };
+
+    person.introduce = newPerson => {
+    return `Hi ${newPerson}, my name is ${name} and I am ${age}!`;
+  };
+  return person;
 };
 
 module.exports = {
